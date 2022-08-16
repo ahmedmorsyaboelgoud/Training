bad46fdc76d68114fd396bdb6a4fc4593af87b18
AhmedMorsy <74056820+ahmedmorsyaboelgoud@users.noreply.github.com>
2022-08-16 22:16:59 +0200

    Add files via upload

---
+++ Moodle Tasks/7.js
@@ -0,0 +1,7 @@
+let num1=Number(prompt("Enter positive number"))
+if(num1%5==0 || num1%8==0){
+    console.log("Number is a multiple of 5 or 8")
+}
+else{
+    console.log("Number isn't a multiple of 5 or 8")
+}
---
+++ Moodle Tasks/8.js
@@ -0,0 +1,13 @@
+let num1=Number(prompt())
+let num2=Number(prompt())
+let num3=Number(prompt())
+
+if(num1 > num2 && num1 > num3){
+console.log("Number1 is the largest")
+}
+else if(num2 > num1 && num2 > num3){
+console.log("Number2 is the largest")
+}
+else{
+    console.log("Number3 is the largest")
+}
