84f27f79f0662203cb799c90988b427131d745cf
AhmedMorsy <74056820+ahmedmorsyaboelgoud@users.noreply.github.com>
2022-08-16 13:55:16 +0200

    Add files via upload

---
+++ Summer Tasks/Task 1,2/Task1.js
@@ -0,0 +1,6 @@
+var x 
+var x =prompt()
+
+var y = (x*10)/100 ;
+x=x-y 
+console.log(x)
---
+++ Summer Tasks/Task 1,2/Task2.js
@@ -0,0 +1,11 @@
+let x= Number(prompt("Enter First Number"));
+let y= Number(prompt("Enter Second Number"));
+let sum1 =x+y;
+let sum2=x/y;
+let sum3= x*y ;
+let sum4= x-y;
+
+console.log(sum1)
+console.log(sum2)
+console.log(sum3)
+console.log(sum4)
