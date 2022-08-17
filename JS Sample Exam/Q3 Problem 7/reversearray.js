b5898f995d982a9441aada8120890dec88a1552b
AhmedMorsy <74056820+ahmedmorsyaboelgoud@users.noreply.github.com>
2022-08-17 16:14:28 +0200

    Delete JS Sample Exam directory

--- JS Sample Exam/Q3 Problem 7/reversearray.js
+++
@@ -1,9 +0,0 @@
-function reverse(x){
-    var y =new Array
-    for(var i =x.length-1 ; i >= 0 ; i--){
-        y.push(x[i])
-    }
-    return y ;
-}
-var arr = [1,2,3,4]
-console.log(reverse(arr))
